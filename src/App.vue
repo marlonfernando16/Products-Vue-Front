2f46389ac4dc432f43946c89ebc063517a37d437
marlonfernando16 <marlon.fernando16@outlook.com>
2020-08-24 15:25:22 -0300

    Create main page with product listing

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Produtos Front</a>
+      </div>
+    </nav>
+
+    <div class="container">
+      <form>
+
+          <label>Nome</label>
+          <input type="text" placeholder="Nome">
+          <label>Quantidade</label>
+          <input type="number" placeholder="QTD">
+          <label>Valor</label>
+          <input type="text" placeholder="Valor">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>QTD</th>
+            <th>VALOR</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr v-for="product of products" :key="product.id">
+
+            <td>{{ product.name }}</td>
+            <td>{{ product.quantity }}</td>
+            <td>{{ product.price }}</td>
+            <td>
+              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+  import Product from './services/products'
+  
+  export default {
+
+    data() {
+      return {
+        products: []
+      }
+    },
+    
+    mounted() {
+      Product.list().then(res => {
+        this.products = res.data;
+      })
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
